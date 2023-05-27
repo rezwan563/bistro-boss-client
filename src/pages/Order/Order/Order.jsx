207c68aba26af6131aad6959bdc17cdd21c69f67
Rezwan <t.rewan17@gmail.com>
2023-05-27 14:08:08 +0600

    Order page react tabs

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import orderBanner from "../../../assets/shop/banner2.jpg";
 import Cover from "../../Shared/Cover/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import useMenu from "../../../hooks/useMenu";
+import OrderTab from "../OrderTab/OrderTab";
 const Order = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = useMenu();
+  const desserts = menu.filter((i) => i.category === "dessert");
+  const pizza = menu.filter((i) => i.category === "pizza");
+  const soup = menu.filter((i) => i.category === "soup");
+  const salad = menu.filter((i) => i.category === "salad");
+  const drinks = menu.filter((i) => i.category === "drinks");
   return (
     <div>
       <Cover img={orderBanner} title="Order Food"></Cover>
-      <div className="tabs">
-        <a className="tab tab-bordered">Tab 1</a>
-        <a className="tab tab-bordered tab-active">Tab 2</a>
-        <a className="tab tab-bordered">Tab 3</a>
-      </div>
+      <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soup</Tab>
+          <Tab>Dessert</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+        <TabPanel>
+          <OrderTab items={salad}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={pizza}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={soup}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={desserts}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={drinks}></OrderTab>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
